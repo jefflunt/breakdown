990d4daf1e851255095ab81d556a843410169aa6
jefflunt <jefflunt@gmail.com>
2026-04-25 20:50:59 -0500

    fix: update import paths in build scripts

--- script/curl-install.sh
+++ script/curl-install.sh
@@ -22,18 +22,9 @@ fi
 
 # We need a tag
 if [ -z "$1" ]; then
-    echo "Usage: curl -sL https://raw.githubusercontent.com/jefflunt/github.com/jefflunt/breakdown/main/script/curl-install.sh | bash -s <tag>"
-    echo "Example tag: b1"
-    exit 1
-fi
-TAG=$1
+    echo "Usage: curl -sL https://raw.githubusercontent.com/jefflunt/breakdown/main/script/curl-install.sh | bash -s <tag>"
+URL="https://github.com/jefflunt/breakdown/releases/download/${TAG}/${BINARY_NAME}"
 
-BINARY_NAME="breakdown-${OS}-${ARCH}"
-if [ "$OS" == "windows" ]; then
-    BINARY_NAME="${BINARY_NAME}.exe"
-fi
-
-URL="https://github.com/jefflunt/github.com/jefflunt/breakdown/releases/download/${TAG}/${BINARY_NAME}"
 
 echo "Downloading ${BINARY_NAME} from ${URL}..."
 
